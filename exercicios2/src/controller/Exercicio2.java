16ef18256b351b81977021799bbf5abc13b59bf7
ESMagalhaes <enzos@NoteEnzo>
2025-02-16 15:06:48 -0300

    enviando 2 exercício

--- exercicios2/src/controller/Exercicio2.java
+++ exercicios2/src/controller/Exercicio2.java
@@ -7,14 +7,14 @@ public class Exercicio2 {
 	}
 	public int findLessValue (int[] vt, int size, int lessAtual) {
 		if (size == 0) {
-			return lessAtual; //se o vetor só possue um espaço (de índice 0), só é necessário retornar o valor nesta posição, pois é o único
+			return lessAtual; //se o vetor só possue um espaço (de índice 0), só é necessário retornar o valor nesta posição, pois é o único, com esta sendo a Condição de Parada
 		}
 		
-		if(vt[size - 1] < lessAtual) { //o vetor precisa ser determinado por [size - 1] pois o índice começa a ser contado do zero, se não o sistema tentaria ler uma posição inexistente
+		else if(vt[size - 1] < lessAtual) { //o vetor precisa ser determinado por [size - 1] pois o índice começa a ser contado do zero, se não o sistema tentaria ler uma posição inexistente
 			lessAtual = vt[size - 1]; //se o  valor no vetor for menor que o menor valor atual, este se tornará o novo menor valor para as próximas comparações.
 		}
 		
-		return findLessValue(vt, size - 1, lessAtual); //retorna o valor, o tamanho menos uma posição para ir decrescendo e o menor valor atualmente;
+		return findLessValue(vt, size - 1, lessAtual); //retorna o valor, o tamanho menos uma posição para ir decrescendo até a Condição de Parada e o menor valor atualmente;
 	}
 
 }
